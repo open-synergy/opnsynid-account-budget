6dd8658502136014cb4912e92d9e0d22c492c87c
Andhitia Rama <andhitia.r@gmail.com>
2020-07-09 15:25:38 +0700

    [8.0.1.0.0] budget_app_cash

---
+++ budget_app_cash/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Budget Management - Cash",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "budget_app",
+        "account_cash_flow_code",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/budget_budget_views.xml",
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
---
+++ budget_app_cash/models/budget_detail.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class BudgetDetail(models.Model):
+    _name = "budget.detail"
+    _inherit = "budget.detail"
+
+    @api.depends(
+        "cash_ids",
+        "cash_ids.amount",
+    )
+    @api.multi
+    def _compute_amount_cash(self):
+        for document in self:
+            total = 0.0
+            for cash in document.cash_ids:
+                total += cash.amount
+            document.amount_cash = total
+
+    amount_cash = fields.Float(
+        string="Amount Cash",
+        compute="_compute_amount_cash",
+        store=True,
+    )
+
+    cash_ids = fields.One2many(
+        string="Cash Realization",
+        comodel_name="budget.detail_cash",
+        inverse_name="detail_id",
+    )
